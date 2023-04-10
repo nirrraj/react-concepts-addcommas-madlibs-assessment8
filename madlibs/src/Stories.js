c2a89362406e10e9f577762c8b5d51c98ae47292
Niraj Singh <119704924+nirrraj@users.noreply.github.com>
2023-04-10 18:32:20 -0400

    Create Stories.js

---
+++ madlibs/src/Stories.js
@@ -0,0 +1,24 @@
+const stories = {
+    fantasy: {
+        start: "Once upon a time there was ",
+        middle: "who tried to fight ",
+        end: "and lost."
+    },
+    romance: {
+        start: "There was once ",
+        middle: "who loved ",
+        end: "and they lived happily ever after."
+    },
+    horror: {
+        start: "On a cold dark night ",
+        middle: "murded ",
+        end: "and to this day you can still hear the screams on nights like this."
+    },
+    comedy: {
+        start: "What did ",
+        middle: "say to ",
+        end: "? ... I don't know!"
+    }
+}
+
+export default stories;
